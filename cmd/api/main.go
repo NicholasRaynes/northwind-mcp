f43019e098292617a63d863a42289793fa898d02
NicholasRaynes <nicholasraynesmb@gmail.com>
2025-11-03 22:34:09 -0600

    Remove static OpenAPI schema endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -46,7 +46,6 @@ func main() {
 	r.GET("/analytics/employee-performance", handlers.GetEmployeePerformance)
 	r.GET("/analytics/shipping-costs", handlers.GetShippingCosts)
 	r.GET("/analytics/delivery-times", handlers.GetDeliveryTimes)
-	r.StaticFile("/openapi.json", "./schema/openapi.json")
 
 	err = r.Run(":" + port)
 	if err != nil {
