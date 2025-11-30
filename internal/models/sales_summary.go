63966a8db96914d860fe073b21bb27d696738983
NicholasRaynes <nicholasraynesmb@gmail.com>
2025-11-30 14:55:15 -0600

    Updated comment for sales_summary group key field

--- internal/models/sales_summary.go
+++ internal/models/sales_summary.go
@@ -1,7 +1,7 @@
 package models
 
 type SalesSummary struct {
-	GroupKey   string  `json:"group_key" db:"group_key"` // e.g., Country
+	GroupKey   string  `json:"group_key" db:"group_key"` // Value dependent on the summary type
 	TotalSales float64 `json:"total_sales" db:"total_sales"`
 	OrderCount int     `json:"order_count" db:"order_count"`
 }
